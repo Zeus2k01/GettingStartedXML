a75d5d180be694ec9afba1a5cb683b18f30bbf5b
Zeus_2k01 <kayoze2006@gmail.com>
2020-11-01 10:42:29 +0530

    added new change

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+	System.out.println("testing");
         setContentView(R.layout.activity_main)
     }
 }
